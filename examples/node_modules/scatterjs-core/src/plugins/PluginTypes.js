0faf805cee13884598e52d42bc8074826bffa10c
chris-allnutt <chris@systemiclabs.com>
2019-03-12 16:34:45 -0400

    Remove node modules, add gititnore, rename repo and package, cleanup

--- examples/node_modules/scatterjs-core/src/plugins/PluginTypes.js
+++
@@ -1,2 +0,0 @@
-export const BLOCKCHAIN_SUPPORT = 'blockchain_support';
-export const WALLET_SUPPORT = 'wallet_support';
--- node_modules/eslint-plugin-no-exclusive-tests/.vimrc
+++
@@ -1,2 +0,0 @@
-let $PATH = './node_modules/.bin:' . $PATH
-let g:syntastic_javascript_checkers = ['eslint']
